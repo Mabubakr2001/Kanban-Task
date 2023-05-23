9f21f02d15e36762b6c6343c95d26887615152f5
Mohamed Aboubakr <mabobakr365@gmail.com>
2023-05-23 21:00:52 +0300

    Added all source code

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+"use-strict";
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  // mode: "production",
+  entry: {
+    index: path.resolve(__dirname, "./src/js/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].[contenthash].js",
+    clean: true,
+    assetModuleFilename: "assets/images/[name].[ext]",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    hot: true,
+    open: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Kanban Task",
+      filename: "index.html",
+      template: "./src/index.html",
+      chunks: ["index"],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/assets"),
+          to: path.resolve(__dirname, "./dist/assets"),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({ filename: "styles/style.css" }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
