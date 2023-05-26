34797fd92bf78beb36a5a8c51162f6b6d7a10314
Mohamed Aboubakr <mabobakr365@gmail.com>
2023-05-26 23:47:26 +0300

    Did some updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 import "../sass/style.scss";
+const boardCreationSpot = document.querySelector(".board-creation");
+const hideSidebarBtn = document.querySelector(".hide-sidebar-btn");
+const showSidebarBtn = document.querySelector(".show-sidebar-btn");
+
+hideSidebarBtn.addEventListener("click", () => {
+  boardCreationSpot.dataset.state = "hidden";
+  showSidebarBtn.dataset.state = "visible";
+});
+
+showSidebarBtn.addEventListener("click", ({ target }) => {
+  boardCreationSpot.dataset.state = "visible";
+  target.dataset.state = "hidden";
+});
